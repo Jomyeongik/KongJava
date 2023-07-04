206b98e27839267cf09c3ddbaf32a46af85d7779
Jomyeongik <mic937267@gmail.com>
2023-07-04 17:19:19 +0900

    [ADD] 23/07/04	OOP 상속과 다형성, 오버라이딩
    1. 상속 개념 및 실습
    	- 상속을 사용하는 이유
    2. 다형성 개념이해 및 실습
    	- 업캐스팅, 다운캐스팅
    	- instanceof 연산자 실습
    3. 오버라이딩 개념 및 연습문제

---
+++ KongJava/src/com/kh/day07/oop/polymorphism/instnceof/Exam_instanceOf.java
@@ -0,0 +1,40 @@
+package com.kh.day07.oop.polymorphism.instnceof;
+
+public class Exam_instanceOf {
+	
+	static void print(Person person) {
+		if(person instanceof Student && person instanceof StudentWorker) {
+			System.out.println("Student입니다!");			
+		}
+		if(person instanceof Researcher) {
+			System.out.println("Researcher입니다!");
+		}
+		if(person instanceof StudentWorker) {
+			System.out.println("StudentWorker입니다!");
+		}
+		// person = new Student();
+		// person = new Researcher();
+		// person = new StudentWorker();
+		// person = new Professor();
+	}
+	
+//	static void print(Researcher researcher) {
+//		System.out.println("Researcher입니다!");
+//	}
+//	static void print(StudentWorker studentWorker) {
+//		System.out.println("StudentWorker입니다!");
+//	}
+//	
+//	static void print(Professor professor) {
+//		System.out.println("Professor입니다.");
+//	}
+//	
+	public static void main(String[] args) {
+
+		print(new Student());
+		print(new Researcher());
+		print(new StudentWorker());
+		print(new Professor());
+	}
+
+}
---
+++ KongJava/src/com/kh/day07/oop/polymorphism/overriding/Line.java
@@ -0,0 +1,11 @@
+package com.kh.day07.oop.polymorphism.overriding;
+
+public class Line extends Shape{
+
+	@Override
+	public void draw() {
+		System.out.println("Line");
+	}
+	
+}
+
